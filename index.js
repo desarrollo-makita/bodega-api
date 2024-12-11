a92f859ae074724facfa5482583b1c955727cc76
jherrera <jherrera@makita.cl>
2024-12-11 15:35:02 -0300

    bug del largo

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ const cors = require('cors');
 const PORT = process.env.PORT || 4000;
 const SECRET_KEY = process.env.SECRET_KEY || 'makita-ti-chile';
 
-app.use(express.json());
 // Middleware para habilitar CORS
 app.use(cors({
   origin: 'http://localhost:4200', // Ajusta esto según el origen permitido
   methods: 'GET,POST,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization',
 }));
-const bodyParser = require('body-parser');
 
-// Incrementar el límite de tamaño
-app.use(bodyParser.json({ limit: '100mb' })); // Ajusta el tamaño según sea necesario
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+// Middleware para manejar solicitudes grandes (de tamaño 100mb)
+app.use(express.json({ limit: '100mb' })); // Asegúrate de que esta línea esté antes de las rutas
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
 // Configurar rutas de la API
 app.use('/api', routes);
 
